c211d47e73b2387c84396d7746e6a20328fade4b
Rohit Singh <conankmails@gmail.com>
2020-04-28 03:59:22 +0530

    commit_2:categories and toolbar completed + basicEmer Activity

--- app/src/main/java/com/example/emercall/MainActivity.java
+++ app/src/main/java/com/example/emercall/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.emercall;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -20,6 +27,34 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<Integer> colors = new ArrayList<>();
     ArrayList<Integer> Icons = new ArrayList<>();
 
+    //Exit from the app if back pressed from home screen
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.setting_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.exit:
+                MainActivity.super.onBackPressed();
+                return true;
+//            case R.id.about:
+//                Intent i =new Intent(MainActivity.this,info.class);
+//                startActivity(i);
+//                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,32 +63,67 @@ public class MainActivity extends AppCompatActivity {
 
         gridMain = findViewById(R.id.gridMain);
         myAdap = new Myadapter();
+        gridMain.setAdapter(myAdap);
 
-        data.add("PRIMARY");
+        //CATEGORIES
+        data.add("BASIC EMERCALLS");
         data.add("ENQUIRY");
         data.add("ACCIDENTS");
         data.add("WOMEN SAFETY");
         data.add("CHILD SAFETY");
-        data.add("FOREIGN");
-
+        data.add("FOREIGNERS");
 
         //COLORS
+        colors.add(R.color.flora);
+        colors.add(R.color.greenCard);
+        colors.add(R.color.Yellow);
         colors.add(R.color.OrangeCard);
         colors.add(R.color.PinkCard);
-        colors.add(R.color.Yellow);
-        colors.add(R.color.greenCard);
-        colors.add(R.color.flora);
         colors.add(R.color.LightYellow);
 
         //ICONS
-        Icons.add(R.drawable.nat_emer_no);
-        Icons.add(R.drawable.default_avatar);
-        Icons.add(R.drawable.default_avatar);
-        Icons.add(R.drawable.women_helpline_no);
-        Icons.add(R.drawable.default_avatar);
-        Icons.add(R.drawable.default_avatar);
+        Icons.add(R.drawable.primary);
+        Icons.add(R.drawable.enquiry);
+        Icons.add(R.drawable.accidents);
+        Icons.add(R.drawable.women_safety);
+        Icons.add(R.drawable.child);
+        Icons.add(R.drawable.foreigner);
 
-        gridMain.setAdapter(myAdap);
+        //onclick
+        gridMain.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch(position)
+                {
+                    case 0:
+                        Intent i = new Intent(getApplicationContext(),categ_basic.class);
+                        startActivity(i);
+                        break;
+                    case 1:
+                        Intent j = new Intent(MainActivity.this,enquiry.class);
+                        startActivity(j);
+                        break;
+                    case 2:
+                        Intent k = new Intent(MainActivity.this,accidents.class);
+                        startActivity(k);
+                        break;
+                    case 3:
+                        Intent l = new Intent(MainActivity.this,categ_women.class);
+                        startActivity(l);
+                        break;
+                    case 4:
+                        Intent m = new Intent(MainActivity.this,children.class);
+                        startActivity(m);
+                        break;
+                    case 5:
+                        Intent n = new Intent(MainActivity.this,foreigner.class);
+                        startActivity(n);
+                        break;
+                    default:
+                        Toast.makeText(MainActivity.this, "Select a valid category", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
 
     }
 
---
+++ app/src/main/java/com/example/emercall/categ_basic.java
@@ -0,0 +1,151 @@
+package com.example.emercall;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class categ_basic extends AppCompatActivity {
+    GridView gridBasic;
+    MyAdapter myAdapter;
+    ArrayList<String> text = new ArrayList<>();
+    ArrayList<Integer> colors = new ArrayList<>();
+    ArrayList<Integer> Icons = new ArrayList<>();
+    ArrayList<String> num = new ArrayList<>();
+    private static final int REQUEST_CALL = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_categ_basic);
+
+        gridBasic = findViewById(R.id.gridBasic);
+        myAdapter = new MyAdapter();
+        gridBasic.setAdapter(myAdapter);
+
+
+        //Numbers
+        text.add("National Emergency Number");
+        text.add("Police");
+        text.add("Fire");
+        text.add("Ambulance");
+        text.add("Medical Helpline");
+        //text.add("FOREIGNERS");
+
+        //Numbers
+        num.add("112");
+        num.add("100");
+        num.add("101");
+        num.add("102");
+        num.add("108");
+
+        //COLORS
+        colors.add(R.color.PinkCard);
+        colors.add(R.color.OrangeCard);
+        colors.add(R.color.greenCard);
+        colors.add(R.color.Yellow);
+        colors.add(R.color.flora);
+        //colors.add(R.color.LightYellow);
+
+        //ICONS
+        Icons.add(R.drawable.nat_emer_no);
+        Icons.add(R.drawable.police);
+        Icons.add(R.drawable.fire);
+        Icons.add(R.drawable.ambulance);
+        Icons.add(R.drawable.medical_helpline);
+       // Icons.add(R.drawable.foreigner);
+
+        gridBasic.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                makeCall(position);
+            }
+        });
+
+    }
+
+    class MyAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return text.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View v = LayoutInflater.from(getApplicationContext()).inflate(R.layout.layout_categories,null);
+            ImageView itemIcon = v.findViewById(R.id.itemIcon);
+            TextView itemName = v.findViewById(R.id.itemName);
+            int pos = position%colors.size();
+            itemIcon.setBackgroundColor(getResources().getColor(colors.get(pos)));
+            itemName.setBackgroundColor(getResources().getColor(colors.get(pos)));
+            itemName.setText(text.get(position)+"\n"+num.get(position));
+            itemIcon.setImageResource(Icons.get(position));
+            return v;
+        }
+    }
+
+    //Code for making a checking permission and making phone call from the app
+    void makeCall(int position)
+    {
+        String no = num.get(position);
+
+        //code for checking permission for making phone calls
+        if(no.trim().length() > 0){
+            if(ContextCompat.checkSelfPermission(categ_basic.this,
+                    Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
+
+                //Code for checking manifest permission
+                ActivityCompat.requestPermissions(categ_basic.this,
+                        new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CALL);
+
+            }else{
+
+                //Code for making call
+                String dial =  "tel:" + no;
+                startActivity(new Intent(Intent.ACTION_CALL, Uri.parse(dial)));
+            }
+        }else{
+            Toast.makeText(categ_basic.this,"Error!\nNumber no exists",Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    //To check id permission is already granted or not
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == REQUEST_CALL){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            }else{
+                Toast.makeText(categ_basic.this, "Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
